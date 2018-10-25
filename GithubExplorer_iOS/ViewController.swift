ec4d89be89e2b9842c7fb21fa9da7540b6971c48
Day, Jeff @ Dallas <jeff.day@cbre.com>
2018-10-25 15:46:01 -0500

    Initial Commit

---
+++ GithubExplorer_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubExplorer_iOS
+//
+//  Created by Day, Jeff @ Dallas on 10/25/18.
+//  Copyright © 2018 Day, Jeff @ Dallas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
